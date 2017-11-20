25e7d446a31b373bdf6bad119490b0dffb6e031b
Henning Waack (waa) <Henning.Waack@softwareag.com>
2017-11-20 14:39:30 +0100

    changed overview page to display correctly on IS homepage

--- is_packages/WxPlatformMonitoring/pub/js/wxplatformmonitoring-onload.js
+++ is_packages/WxPlatformMonitoring/pub/js/wxplatformmonitoring-onload.js
@@ -29,48 +29,50 @@ function appendScript(url, $parent) {
   $parent.append( script );
 }
 
-$(window).on("load", function() {
-    if (extSettings_showOnStatusPage !== true) {
-        console.log("WxPlatformMonitoring: Extended settings 'watt.wx.platformMonitoring.healthPortlet.showOnStatusPage' is set to '" + extSettings_showOnStatusPage + "', there not including WxPlatformMonitoring health page");
-        return;
-    }
-    if( extSettings_importHealthPagePath === null ) {
-      extSettings_importHealthPagePath = "../WxPlatformMonitoring/wxplatformmonitoring_healthpage.html";
-    }
-    $wxIFrame = $('<iframe>', {
-        src: extSettings_importHealthPagePath,
-        width: '100%',
-        id: 'WxPlatformMonitoringIFrame',
-        frameborder: 0,
-        scrolling: 'no',
-    });
-    // get the body of the main frame, i.e. the frame with the is-stats.dsp
-    var $mainBody = $(window.parent.frames[0].frames[2].document);
-    //
-    // $('head', $mainBody).append('<link rel="stylesheet" type="text/css" href="/WxPlatformMonitoring/css/pure-nr-min.css" />');
-    // $('head', $mainBody).append('<link rel="stylesheet" type="text/css" href="/WxPlatformMonitoring/css/bootstrap/bootstrap.min.css" />');
-    // $('head', $mainBody).append('<link rel="stylesheet" type="text/css" href="/WxPlatformMonitoring/css/json-formatter.css" />');
-    // $('head', $mainBody).append('<link rel="stylesheet" type="text/css" href="/WxPlatformMonitoring/css/wxplatformmonitoring.css" />');
-    // $('head', $mainBody).append('<link rel="stylesheet" type="text/css" href="/WmRoot/webMethods.css" />');
-    //
-    // appendScript("/WxPlatformMonitoring/js/jquery-3.1.1.min.js", $('head', $mainBody));
-    // appendScript("/WxPlatformMonitoring/js/bootstrap.min.js", $('head', $mainBody));
-    // appendScript("/WxPlatformMonitoring/js/bootbox.min.js", $('head', $mainBody));
-    // appendScript("/WxPlatformMonitoring/js/json-formatter.js", $('head', $mainBody));
 
-    // $('head', $mainBody).append('<script src="/WxPlatformMonitoring/js/jquery-3.1.1.min.js"></' + 'script>');
-    // $('head', $mainBody).append('<script src="/WxPlatformMonitoring/js/bootstrap.min.js"></' + 'script>');
-    // $('head', $mainBody).append('<script src="/WxPlatformMonitoring/js/bootbox.min.js"></' + 'script>');
-    // $('head', $mainBody).append('<script src="/WxPlatformMonitoring/js/wxplatformmonitoring.js"></' + 'script>');
-    // $('head', $mainBody).append('<script src="/WxPlatformMonitoring/js/json-formatter.js"></' + 'script>');
-    //
-    // $b = $('body', $mainBody);
-    //   $('<script>alert("hi");</' + 'script>').appendTo($b);
 
-    $('body', $mainBody).append($wxIFrame);
-    // $.get(extSettings_importHealthPagePath, function(data){
-    //   $('body', $mainBody).append(data);
-    // });
-    // appendScript("/WxPlatformMonitoring/js/wxplatformmonitoring.js", $('head', $mainBody));
-    //setIframeHeight($wxIFrame);
+
+$(window).on("load", function() { 
+	
+		if (extSettings_showOnStatusPage !== true) {
+		console.log("WxPlatformMonitoring: Extended settings 'watt.wx.platformMonitoring.healthPortlet.showOnStatusPage' is set to '" + extSettings_showOnStatusPage + "', there not including WxPlatformMonitoring health page");
+			return;
+		}
+		if( extSettings_importHealthPagePath === null ) {
+			extSettings_importHealthPagePath = "/WxPlatformMonitoring/wxplatformmonitoring_healthpage.html";
+		}
+	  var $mainBody = $(window.parent.frames[0].frames[2].document);
+		var $body = $('body', $mainBody);
+		var $div = $('<div>');
+//		var $table = $('table:first', $body);
+//		var $tbody = $('tbody:first', $table);
+//		var $td = $('<td colspan="3" valign="top">');
+//		var $tr = $('<tr>').append($td);
+//		$tbody.append($tr);
+		$body.append($div);
+		$div.html('<object data="' + extSettings_importHealthPagePath + '" width="100%" height="100%">');
+		
+		//		$('body', $mainBody).children("#henning").html('<object data="/WxPlatformMonitoring/wxplatformmonitoring_healthpage.html" width="100%" height="100%">');
+//		$('body', $mainBody).children("#henning").load('WxPlatformMonitoring/wxplatformmonitoring_healthpage.html');
+//    $('#siteloader').load('http://www.somesitehere.com');
+	
 });
+//$(window).on("load", function() {
+//	if (extSettings_showOnStatusPage !== true) {
+//		console.log("WxPlatformMonitoring: Extended settings 'watt.wx.platformMonitoring.healthPortlet.showOnStatusPage' is set to '" + extSettings_showOnStatusPage + "', there not including WxPlatformMonitoring health page");
+//		return;
+//	}
+//	if( extSettings_importHealthPagePath === null ) {
+//		extSettings_importHealthPagePath = "../WxPlatformMonitoring/wxplatformmonitoring_healthpage.html";
+//	}
+//	$wxIFrame = $('<iframe>', {
+//		src: extSettings_importHealthPagePath,
+//		width: '100%',
+//		id: 'WxPlatformMonitoringIFrame',
+//		frameborder: 0,
+//		scrolling: 'no',
+//	});
+//	// get the body of the main frame, i.e. the frame with the is-stats.dsp
+//	var $mainBody = $(window.parent.frames[0].frames[2].document);
+//	$('body', $mainBody).append($wxIFrame);
+//});
