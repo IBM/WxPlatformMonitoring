a67e95374064d2855085ec314cdb06729027c314
Thomas Rehm <thomas.rehm@softwareag.com>
2023-11-21 08:30:24 +0100

    removed outdated comments

--- is_packages/WxPlatformMonitoring/code/source/wx/platformMonitoring/impl/internal/utility/logging.java
+++ is_packages/WxPlatformMonitoring/code/source/wx/platformMonitoring/impl/internal/utility/logging.java
@@ -108,26 +108,7 @@ public final class logging
 	{
 		// --- <<IS-START(startup)>> ---
 		// @sigtype java 3.5
-		// In order to use log4j in a customer package it is required to:
-		//   1. copy log4j-api.jar,log4j-core.jar to WxPlatformMonitoring\code\jars\
-		//   2. enable package classloader by adding this line to the manifest.v3 file 
-		//        <value name="classloader">package</value>
-		//
-		// How to trace Configurator.initialize()
-		//
-		//   1. set java parameter: -Dlog4j.debug (e.g. in the custom_wrapper.conf)
-		//
-		//   2. with the above paramater you should see debug messages in the wrapper.log like if you invoke wx.platformMonitoring.impl.internal.startup:setupLogger
-		//   INFO   | jvm 1    | 2023/11/20 11:07:13 | wx.platformMonitoring.impl.internal.startup:setupLogger - Initializing log4j configuration file: packages/WxPlatformMonitoring/config/log4j2.xml ...
-		//   ...                                       here you should see a lot of 'StatusLogger' messages
-		//   INFO   | jvm 1    | 2023/11/20 11:07:14 | DEBUG StatusLogger LoggerContext[name=Default, org.apache.logging.log4j.core.LoggerContext@5d26023c] started OK with configuration XmlConfiguration[location=C:\SoftwareAG1015\wm\IntegrationServer\instances\default\packages\WxPlatformMonitoring\config\log4j2.xml].
-		//   INFO   | jvm 1    | 2023/11/20 11:07:14 | wx.platformMonitoring.impl.internal.startup:setupLogger - Initializing log4j configuration file: packages/WxPlatformMonitoring/config/log4j2.xml completed
-		
-		//   3. If you invoke wx.platformMonitoring.impl.internal.startup:setupLogger a second time the init will not work, you will just see these log records
-		//   INFO   | jvm 1    | 2023/11/20 11:07:13 | wx.platformMonitoring.impl.internal.startup:setupLogger - Initializing log4j configuration file: packages/WxPlatformMonitoring/config/log4j2.xml ...
-		//   INFO   | jvm 1    | 2023/11/20 11:07:14 | wx.platformMonitoring.impl.internal.startup:setupLogger - Initializing log4j configuration file: packages/WxPlatformMonitoring/config/log4j2.xml completed
-		//   Reason: log4j ignores subsequent Configurator.initialize() calls for the same log4j config file. This is done by registering the configuration file to the calling class (in our case wx.platformMonitoring.impl.internal.startup).
-		//   Workaround: rebuild the class and invoke wx.platformMonitoring.impl.internal.startup:setupLogger. During rebuild the configuration file is "unregistered".
+		// Note: add java parameter -Dlog4j.debug (in the custom_wrapper.conf) to debug log4j. The debug records are logged to the wrapper.log
 				
 		File log4jConfigFile = new File(LOG4J_CFG);
 		
@@ -154,6 +135,7 @@ public final class logging
 		} else {
 			throw new ServiceException("Configuration file '" + LOG4J_CFG + "' does not exist or cannot be read");
 		}		
+			
 		// --- <<IS-END>> ---
 
                 
@@ -163,6 +145,7 @@ public final class logging
 	private static final String PACKAGE_NAME = "WxPlatformMonitoring";
 	private static final String LOG4J_CFG = "./packages/" + PACKAGE_NAME + "/config/log4j2.xml";
 	private static LoggerContext logCtx  = null;
+		
 	// --- <<IS-END-SHARED>> ---
 }
 
